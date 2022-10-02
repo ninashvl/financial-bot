f452b835b66092da648f30d2df2a0cd33d65f217
Павел Лукьянцев <lukyantsev-pa@mail.ru>
2022-10-02 21:26:46 +0000

    Revert "Merge branch 'GOHW-1' into 'master'"
    This reverts merge request !42

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,25 +1,3 @@
 package main
 
-import (
-	"log"
-
-	"gitlab.ozon.dev/lukyantsev-pa/lukyantsev-pavel/internal/clients/tg"
-	"gitlab.ozon.dev/lukyantsev-pa/lukyantsev-pavel/internal/config"
-	"gitlab.ozon.dev/lukyantsev-pa/lukyantsev-pavel/internal/model/messages"
-)
-
-func main() {
-	config, err := config.New()
-	if err != nil {
-		log.Fatal("config init failed:", err)
-	}
-
-	tgClient, err := tg.New(config)
-	if err != nil {
-		log.Fatal("tg client init failed:", err)
-	}
-
-	msgModel := messages.New(tgClient)
-
-	tgClient.ListenUpdates(msgModel)
-}
+func main() {}
