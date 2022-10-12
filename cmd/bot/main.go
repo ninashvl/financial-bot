9091a6e6c1f22717363ba87cec2b722f3bf4503f
ninashvl <ninashvl@gmail.com>
2022-10-12 14:16:14 +0500

    Project Base added

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"gitlab.ozon.dev/ninashvl/homework-1/config"
+	"gitlab.ozon.dev/ninashvl/homework-1/internal/clients/tg"
+	"gitlab.ozon.dev/ninashvl/homework-1/internal/messages"
+)
+
+func main() {
+	config, err := config.New()
+	if err != nil {
+		log.Fatal("config init failed:", err)
+	}
+
+	tgClient, err := tg.New(config)
+	if err != nil {
+		log.Fatal("tg client init failed:", err)
+	}
+
+	msgModel := messages.New(tgClient)
+
+	tgClient.ListenUpdates(msgModel)
+}
